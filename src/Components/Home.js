42c96e5618ee004ec25ef618ee5849ad2438a601
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-11-02 23:11:29 +0530

    initial commit

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/home.css";
+
+function Home() {
+  return (
+    <div className="background-img">
+      <div className="door-waves-one"></div>
+      <div className="door-waves-one door-waves-two"></div>
+    </div>
+  );
+}
+
+export default Home;
